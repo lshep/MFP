412f3b95da1c185a57274830c3e550703a5b8674
lshep <shepherl@gmail.com>
2017-07-26 15:31:32 +0000

    Added Greet Function and Documentation

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' @aliases greet
+#' @aliases hello
+#' @title Greeting A Person
+#' @description Given a person's name, a customized greeting will be given.
+#' @rdname hello
+#' @param name character() Name of person
+#' @return character() Greeting
+#' @examples
+#' greet("Mary")
+#' @export
+greet <- function(name){
+  print(paste("Hello", name))
+}
